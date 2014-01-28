6b13631447b3296b0aee745590bd76d884f88afb
electrosam <sghewari@gmail.com>
2014-01-27 20:42:15 -0800

    Completed double_operand instructions

--- code/double_operand.v
+++ code/double_operand.v
@@ -13,6 +13,7 @@ function double_operand;
 begin
   double_operand=0;
   source_word=read_word(instruction[11:9],instruction[8:6]);
+  destination_word=read_word(instruction[5:3],instruction[2:0]);
   
   case(instruction[15:12])
           MOV:
@@ -27,14 +28,27 @@ begin
           
           CMP:
           begin
-            destination_word=read_word(instruction[5:3],instruction[2:0]);
+            
             result=source_word-destination_word;
-            if(!result)
-              PSW[ZERO]=1;
-            else if(result<0)
-              PSW[NEGATIVE]=1;
-            else if(result[16])
-              PSW[CARRY]=0;
+            if(!(result[15:0]))
+             PSW[ZERO]=1;
+             else 
+             PSW[ZERO]=0;
+             
+            if(result[15])
+             PSW[NEGATIVE]=1;
+           else
+             PSW[NEGATIVE]=0;
+             
+             if(result[16])
+               PSW[CARRY]=1;
+             else
+               PSW[CARRY]=0;
+                if((source_word[15]!=destination_word[15])&&result[15]==source_word[15])
+                 PSW[OVERFLOW]=1;
+               else
+                 PSW[OVERFLOW]=0;
+             
             //Overflow logic goes here
           end
           
@@ -43,27 +57,125 @@ begin
           end
           BIT:
           begin
+            result=source_word & destination_word;
+            
+            if(!(result[15:0]))
+             PSW[ZERO]=1;
+             else 
+             PSW[ZERO]=0;
+             
+            if(result[15])
+             PSW[NEGATIVE]=1;
+           else
+             PSW[NEGATIVE]=0;
+             
+            PSW[OVERFLOW]=0; 
+
           end
           BITB:
           begin
           end
-          BIC:
+          BIC://Bit Clear. Clears bits in Destination corrosponding to set bits in Source.
           begin
+            result=~(source_word)&(destination_word);
+            if(write_word(instruction[5:3],instruction[2:0],result))
+              $display("Error during BIC Instruction");
+              
+              if(!(result[15:0]))
+             PSW[ZERO]=1;
+             else 
+             PSW[ZERO]=0;
+             
+            if(result[15])
+             PSW[NEGATIVE]=1;
+           else
+             PSW[NEGATIVE]=0;
+             
+            PSW[OVERFLOW]=0; 
+            
+            
           end
           BICB:
           begin
           end
           BIS:
-          begin
+          begin//Bit Set. Sets bits in Destination corrosponding to set bits in Source. 
+           begin
+            result=source_word || destination_word;
+             if(write_word(instruction[5:3],instruction[2:0],result))
+              $display("Error during BIS Instruction");
+              
+              if(!(result[15:0]))
+             PSW[ZERO]=1;
+             else 
+             PSW[ZERO]=0;
+             
+            if(result[15])
+             PSW[NEGATIVE]=1;
+           else
+             PSW[NEGATIVE]=0;
+             
+            PSW[OVERFLOW]=0; 
+          end  
+            
           end
           BISB:
           begin
           end
           ADD:
           begin 
+            result=source_word+destination_word;
+            if(write_word(instruction[5:3],instruction[2:0],result))
+            $display("Error during ADD Instruction");
+            
+            if(!(result[15:0]))
+             PSW[ZERO]=1;
+           else 
+             PSW[ZERO]=0;
+             
+            if(result[15])
+             PSW[NEGATIVE]=1;
+           else
+             PSW[NEGATIVE]=0;
+             
+             if(result[16])
+               PSW[CARRY]=1;
+             else
+               PSW[CARRY]=0;
+               
+               if((source_word[15]==destination_word[15])&&result[15]!=source_word[15])
+                 PSW[OVERFLOW]=1;
+               else
+                 PSW[OVERFLOW]=0;
+                 
+
           end
           SUB:
           begin
+             result=destination_word-source_word;
+            if(write_word(instruction[5:3],instruction[2:0],result))
+            $display("Error during SUB Instruction");
+            
+            if(!(result[15:0]))
+             PSW[ZERO]=1;
+           else 
+             PSW[ZERO]=0;
+             
+            if(result[15])
+             PSW[NEGATIVE]=1;
+           else
+             PSW[NEGATIVE]=0;
+             
+             if(result[16])
+               PSW[CARRY]=1;
+             else
+               PSW[CARRY]=0;
+               
+               if((source_word[15]!=destination_word[15])&&result[15]==source_word[15])
+                 PSW[OVERFLOW]=1;
+               else
+                 PSW[OVERFLOW]=0;
+                 
           end
           default:
           begin
